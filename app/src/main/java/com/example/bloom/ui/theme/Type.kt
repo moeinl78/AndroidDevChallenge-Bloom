cbc515a2f8d4f24a2e6f4bcb7b48ecdb85206c9f
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-10-29 20:06:33 +0330

    Add typography for the fonts etc... #1.

--- app/src/main/java/com/example/bloom/ui/theme/Type.kt
+++ app/src/main/java/com/example/bloom/ui/theme/Type.kt
@@ -2,27 +2,44 @@ package com.example.bloom.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.bloom.R
 
-// Set of Material typography styles to start with
+private val NunitoBold = FontFamily(Font(R.font.nunito_sans_bold))
+private val NunitoLight = FontFamily(Font(R.font.nunito_sans_light))
+private val NunitoSemiBold = FontFamily(Font(R.font.nunito_sans_semibold))
+
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 18.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = NunitoLight,
+        fontSize = 13.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+        fontFamily = NunitoLight,
         fontSize = 14.sp
     ),
+    body2 = TextStyle(
+        fontFamily = NunitoLight,
+        fontSize = 12.sp
+    ),
+    button = TextStyle(
+        fontFamily = NunitoSemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp,
+    ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = NunitoSemiBold,
         fontSize = 12.sp
     )
-    */
 )
