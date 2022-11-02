4edce870868f2821c5baba08b102bd706d107443
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-11-02 20:34:35 +0330

    Add CustomButton.kt to be used through each screen #9.

---
+++ app/src/main/java/com/example/bloom/ui/components/CustomButton.kt
@@ -0,0 +1,30 @@
+package com.example.bloom.ui.components
+
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun CustomButton(
+    modifier: Modifier,
+    action: () -> Unit,
+    contentText: String,
+) {
+    Button(
+        onClick = action,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.secondary,
+            contentColor = MaterialTheme.colors.onSecondary
+        ),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Text(
+            text = contentText
+        )
+    }
+}
