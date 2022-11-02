42a6a3c392a0d650ecb87fa920c9315d2179c5c3
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-11-02 23:04:48 +0330

    Add options for each text field #9.

--- app/src/main/java/com/example/bloom/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/bloom/ui/screens/login/LoginScreen.kt
@@ -7,21 +7,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.bloom.R
 import com.example.bloom.ui.components.CustomButton
 import com.example.bloom.ui.theme.BloomTheme
 
@@ -89,15 +100,35 @@ private fun EmailField() {
                 text = "Email Address",
                 fontWeight = FontWeight.Bold
             )
-        }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colors.secondary,
+            focusedLabelColor = MaterialTheme.colors.secondary
+        )
     )
 }
 
 @Composable
 private fun PasswordField() {
-    var password by remember {
+
+    var password by rememberSaveable {
         mutableStateOf("")
     }
+
+    var passwordVisibility by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val icon = if (passwordVisibility) {
+        painterResource(R.drawable.visibility)
+    } else {
+        painterResource(R.drawable.visibility_off)
+    }
+
     OutlinedTextField(
         value = password,
         onValueChange = {
@@ -111,7 +142,26 @@ private fun PasswordField() {
                 text = "Password (+8 characters)",
                 fontWeight = FontWeight.Bold
             )
-        }
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                passwordVisibility = !passwordVisibility
+            }) {
+                Icon(
+                    painter = icon,
+                    contentDescription = null
+                )
+            }
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colors.secondary,
+            focusedLabelColor = MaterialTheme.colors.secondary
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
     )
 }
 
